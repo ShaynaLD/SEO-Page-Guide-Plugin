c2022db39d4ab0c387223ec8d33c6c5397c289f9
ShaynaLD <shaynalernerdiamond@gmail.com>
2018-03-27 08:15:16 -0400

    add initial php file w plugin declaration

---
+++ seo-page-guide.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  SEO Page Guide
+Plugin URI:   
+Description:  This plugin is designed to help website owners see the SEO content added on the back end of the site in the Yoast Plugin without having to go to the back end of the page/site. It will only show on the front end when the user is logged in.
+Version:      0.1
+Author:       ShaynaLD
+Author URI:   
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  seopg
+Domain Path:  /languages
+*/

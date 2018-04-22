627e436d07a23e7258ed06bc2c3ab34cb6c9f145
Shayna <Shayna@Shaynas-Mac-mini.local>
2018-04-21 20:32:22 -0400

    adjust desctiption

--- seo-page-guide.php
+++ seo-page-guide.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:  SEO Page Guide
 Plugin URI:   
-Description:  This plugin is designed to help website owners see the SEO content added on the back end of the site in the Yoast Plugin without having to go to the back end of the page/site. It will only show on the front end when the user is logged in.
+Description:  This plugin is designed to help website owners see the SEO content added on the back end of the site in the Yoast Plugin without having to go to the back end of the page/site. It will only show on the front end when the user is logged in. Designed by Tara Claeys. Developed by Shayna Diamond.
 Version:      0.1
 Author:       ShaynaLD
 Author URI:   
